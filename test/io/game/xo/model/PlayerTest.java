3c206c7850e9466ba9372a690f0c0f153d89305e
artem <artem.horuzhenko@softeq.com>
2018-06-14 11:54:32 +0300

    PlayerTest - created;

--- test/io/game/xo/model/PlayerTest.java
+++ test/io/game/xo/model/PlayerTest.java
@@ -8,9 +8,23 @@ public class PlayerTest {
 
     @Test
     public void getName() {
+        final String inputName = "Alex";
+        final String expectedName = inputName;
+
+        final Player player = new Player(inputName, null);
+        final String actualName = player.getName();
+
+        assertEquals(expectedName, actualName);
     }
 
     @Test
     public void getFigure() {
+        final Figure inputFigure = Figure.X;
+        final Figure expectedFigure = inputFigure;
+
+        final Player player = new Player(null, inputFigure);
+        final Figure actualFigure = player.getFigure();
+
+        assertEquals(expectedFigure, actualFigure);
     }
 }
