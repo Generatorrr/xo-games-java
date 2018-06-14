084dbd7abd90b4b1965ae4fe3a62f5ba5922ef3f
Generatorr <martinkov1994@gmail.com>
2018-06-14 13:40:00 +0300

    initial MoveController

---
+++ src/io/game/xo/controllers/MoveController.java
@@ -0,0 +1,23 @@
+package io.game.xo.controllers;
+
+import io.game.xo.model.Field;
+import io.game.xo.model.Figure;
+import io.game.xo.model.exceptions.AlreadyOccupiedException;
+import io.game.xo.model.exceptions.InvalidPointException;
+
+import java.awt.*;
+
+public class MoveController {
+
+    public void applyFigure(final Field field,
+                            final Point point,
+                            final Figure figure) throws InvalidPointException, AlreadyOccupiedException {
+
+        if (field.getFigure(point) != null) {
+            throw new AlreadyOccupiedException();
+        }
+        field.setFigure(point, figure);
+
+    }
+
+}
